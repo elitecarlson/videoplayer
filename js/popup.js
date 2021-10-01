37dfa3b3f156416b115120c0d8a142659e0a27b2
Elite-Carlson <elitecarlson@gmail.com>
2021-10-01 17:57:43 +0200

    popup for upload

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+const like = document.querySelector(".like");
+const addtoplaylist = document.querySelector(".playlist");
+const menu = document.querySelector(".menu");
+const addvideo = document.querySelector(".upload");
+
+function upload(){
+    if (1==1) {
+        document.documentElement.style.setProperty('--visiblity', 'visible');
+        document.documentElement.style.setProperty('--zindex', '1');
+    }else if(1==0){
+
+    }
+}
+
+addvideo.addEventListener('click', upload);
