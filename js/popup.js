d7fbb8e00ab66fc511778222e71d57eb5b04f209
Elite-Carlson <elitecarlson@gmail.com>
2021-10-02 13:10:07 +0200

    cancel button fucntioning

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,7 @@ const like = document.querySelector(".like");
 const addtoplaylist = document.querySelector(".playlist");
 const menu = document.querySelector(".menu");
 const addvideo = document.querySelector(".upload");
+const cancelpopup = document.querySelector(".cancel");
 
 function upload(){
     if (1==1) {
@@ -11,5 +12,10 @@ function upload(){
 
     }
 }
+function closepopup(){
+    document.documentElement.style.setProperty('--visiblity', 'none');
+    document.documentElement.style.setProperty('--zindex', '-1');  
+}
 
 addvideo.addEventListener('click', upload);
+cancelpopup.addEventListener('click', closepopup);
