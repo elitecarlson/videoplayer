2c3148a59dc1327d05e34d636ce3992ffbab9f7e
Elite-Carlson <elitecarlson@gmail.com>
2021-10-04 13:59:07 +0200

    just made it prettier looking no big changes

--- js/popup.js
+++ js/popup.js
@@ -6,6 +6,8 @@ const cancelpopup = document.querySelector(".cancel");
 const dropvid = document.querySelector(".droparea");
 const selectfile = document.querySelector(".uploadimg");
 const videotitle = document.querySelector(".vidtitle");
+const browsefiles = document.querySelector(".browsefile");
+
 
 function upload(){
     if (1==1) {
@@ -15,14 +17,6 @@ function upload(){
     }else if(1==0){
 
     }
-}
-
-function closepopup(){
-    document.documentElement.style.setProperty('--visiblity', 'none');
-    document.documentElement.style.setProperty('--zindex', '-1');
-    document.documentElement.style.setProperty('--bgcolor', 'none'); 
-    document.documentElement.style.setProperty('--borderstyle', 'dotted');
-    selectfile.className = "uploadimg";    
 }
 
 function dragoverarea(){
@@ -39,13 +33,23 @@ function dropvideo(){
     var droppedvideo = event.dataTransfer.files[0];
     var name = droppedvideo.name;
     var actualname = name.split('.');   
+
     if (actualname[1] !== "mp4") {
-        alert("Pleas Upload a video")
+        alert("Please Upload a video")
     }else{     
     document.documentElement.style.setProperty('--borderstyle', 'solid'); 
     selectfile.className = "successimg";
-    videotitle.value = actualname[0]; 
+    videotitle.value = actualname[0];
     }
+}
+
+function closepopup(){  
+    document.documentElement.style.setProperty('--visiblity', 'none');
+    document.documentElement.style.setProperty('--zindex', '-1');
+    document.documentElement.style.setProperty('--bgcolor', 'none'); 
+    document.documentElement.style.setProperty('--borderstyle', 'dotted');
+    videotitle.value = "";
+    selectfile.className = "uploadimg";    
 }
 
 addvideo.addEventListener('click', upload);
@@ -53,4 +57,5 @@ cancelpopup.addEventListener('click', closepopup);
 dropvid.addEventListener('dragover', dragoverarea);
 dropvid.addEventListener('dragleave', dragleavearea);
 dropvid.addEventListener('drop', dropvideo);
+browsefiles.addEventListener('click', dropvideo);
 
