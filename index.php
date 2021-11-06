e63c0277ebfe5f77570a4fa752503edc4d53565a
Elite Carlson <elitecarlson@gmail.com>
2021-11-06 12:27:24 +0200

    removing some bugs from the code

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/navbar.css">
 </head>
 <body>
     
